4d62b068edcc8df5cae8c797ad60f64292af6cd3
Derya <deryaelgit@hotmail.com>
2024-11-19 15:38:53 +0300

    Son

--- obj/Debug/net8.0/Portal.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ddbf171218b62169dd1e62dfe2f4e0fc0b5bb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
